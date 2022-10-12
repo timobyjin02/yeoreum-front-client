4fed6e2b340a64ab56cda67ad601754141455bdf
jomi707 <dol7966@gmail.com>
2022-10-12 14:07:35 +0900

    fix: props 변경(size 타입: number[] -> string[], value -> shadow), 오류 수정(세미콜론, blur '-' 표기)

--- yeoreum/src/common/boxes/components/InnerBox.tsx
+++ yeoreum/src/common/boxes/components/InnerBox.tsx
@@ -1,22 +1,27 @@
 import styled from '@emotion/styled';
 
 interface Props {
-  children: React.ReactNode,
-  size: number[],
-  value: number
+  children: React.ReactNode;
+  size: string[];
+  shadow: number;
 }
 
-function InnerBox(props:Props) {
-  return (  
-    <Wrapper size={props.size} value={props.value} >{props.children}</Wrapper>
-  )
+function InnerBox({ ...props }: Props) {
+  return (
+    <>
+      <Wrapper {...props}>{props.children}</Wrapper>
+    </>
+  );
 }
 
 export default InnerBox;
 
-const Wrapper = styled.div<{size: number[], value: number}>`
-  width: ${(props) => props.size[0]}px;
-  height: ${(props) => props.size[1]}px;
-  ${({ theme }) => theme.common.inner};
-  box-shadow: ${({value}) => `inset ${value}px ${value}px ${value*2}px #a3a3a9, inset -${value}px -${value}px -${value*2}px #ffffff`}
+const Wrapper = styled.div<{ size: string[]; shadow: number }>`
+  width: ${props => props.size[0]};
+  height: ${props => props.size[1]};
+  ${({ theme }) => theme.common.inner}
+  box-shadow: ${({ shadow }) =>
+    `${shadow}px ${shadow}px ${
+      shadow * 2
+    }px #afafb5, -${shadow}px -${shadow}px ${shadow}px #ffffff;`}
 `;
