5f667354662569e46ccbc16d0d09973f680c6aa2
donkey_kong <donghui8266@gmail.com>
2022-11-02 17:39:58 +0900

    여름 개같이 부활

--- yeoreum/src/common/styles/emotion.d.ts
+++
@@ -1,9 +0,0 @@
-import "@emotion/react";
-import theme, { PaletteType, CommonType } from "./theme";
-
-declare module "@emotion/react" {
-  export interface Theme {
-    palette: PaletteType;
-    common: CommonType;
-  }
-}
