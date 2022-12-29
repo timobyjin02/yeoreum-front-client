2036631a88dabd627b52c150dbbdeddf5ad0e1a8
bang-koon <dol7966@gmail.com>
2022-12-29 14:03:46 +0900

    feat(feature/bang/#25): 공지사항 컴포넌트 구조 설계, layout 퍼블리싱

---
+++ yeoreum/src/pages/service/notice/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '../../../components/service/board/layout';
+import Posts from '../../../components/service/board/posts';
+
+const Board = () => {
+  return (
+    <>
+      <Layout title="공지사항">
+        <Posts />
+      </Layout>
+    </>
+  );
+};
+
+export default Board;
