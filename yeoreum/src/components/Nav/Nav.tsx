8651243f99ce0ddeec85b1cbc1ade9bd525e8aa2
donkey_kong <donghui8266@gmail.com>
2023-01-04 14:39:42 +0900

    feat(feature/donkey/#32): 브라우저 리사이징 모달 닫기 구현

--- yeoreum/src/components/Nav/Nav.tsx
+++ yeoreum/src/components/Nav/Nav.tsx
@@ -3,22 +3,41 @@ import { useRouter } from 'next/router';
 import ModalPortal from '../ModalPortal/ModalPortal';
 import UserModal from '../UserModal/UserModal';
 import { NavService, NavUsual } from './NavBar';
+import Hamburger from '../UserModal/Hamburger';
+import useResize from '../../hooks/useResize';
 
 function Nav() {
   const [userOpen, setUserOpen] = useState(false);
+  const [hamburger, setHamburger] = useState(false);
   const { pathname } = useRouter();
   const isInService = pathname.slice(0, 8) === '/service';
+
+  useResize('above', 640, () => setHamburger(false));
+  useResize('below', 640, () => setUserOpen(false));
 
   return (
     <>
       {isInService ? (
-        <NavService userOpen={userOpen} setUserOpen={setUserOpen} />
+        <NavService
+          userOpen={userOpen}
+          setUserOpen={setUserOpen}
+          setHamburger={setHamburger}
+        />
       ) : (
-        <NavUsual userOpen={userOpen} setUserOpen={setUserOpen} />
+        <NavUsual
+          userOpen={userOpen}
+          setUserOpen={setUserOpen}
+          setHamburger={setHamburger}
+        />
       )}
       {userOpen && (
         <ModalPortal>
           <UserModal onClose={() => setUserOpen(false)} />
+        </ModalPortal>
+      )}
+      {hamburger && (
+        <ModalPortal>
+          <Hamburger onClose={() => setHamburger(false)} />
         </ModalPortal>
       )}
     </>
