a501501bbab90ed7dbce560bf5a5905f9a510fd0
ujin <dbwlsxkal95@gmail.com>
2023-01-16 19:56:14 +0900

    file(feature/ujin/#39): 마이 페이지 최상위 컴포넌트 Container 삭제

--- yeoreum/src/pages/mypage/index.tsx
+++ yeoreum/src/pages/mypage/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import Container from '../../components/myPage/Container';
+import PostContainer from '../../components/board/PostContainer';
+import PostPageTitle from '../../components/board/PostPageTitle';
+import Lists from '../../components/myPage/Lists';
+import Profile from '../../components/myPage/Profile';
+import Rating from '../../components/myPage/Rating';
 
 function index() {
-  return <Container></Container>;
+  return (
+    <PostContainer>
+      <PostPageTitle title="마이페이지" />
+      <Profile />
+      <Rating />
+      <Lists />
+    </PostContainer>
+  );
 }
 
 export default index;
