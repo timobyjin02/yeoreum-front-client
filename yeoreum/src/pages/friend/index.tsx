f7a1f2774ed1a7c98796db61bb514d6156411021
ujin <dbwlsxkal95@gmail.com>
2023-02-02 11:38:31 +0900

    dir(feature/ujin/#58): 친구 페이지 폴더 생성

---
+++ yeoreum/src/pages/friend/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PostContainer from '../../components/board/PostContainer';
+
+function index() {
+  return <PostContainer></PostContainer>;
+}
+
+export default index;
