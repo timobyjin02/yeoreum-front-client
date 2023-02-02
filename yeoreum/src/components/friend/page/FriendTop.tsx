1ef463a8dba8c1d8fdeb03c1238473efb085466f
ujin <dbwlsxkal95@gmail.com>
2023-02-02 11:47:45 +0900

    publish(feature/ujin/#58): 친구 페이지 UI 구현

---
+++ yeoreum/src/components/friend/page/FriendTop.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import React, { useState } from 'react';
+import PostPageTitle from '../../board/PostPageTitle';
+import Modal from '../../common/Modal';
+// import ApplicationFriendModal from '../applicationModal/ApplicationFriendModal';
+
+function FriendTop() {
+  const [isOpen3, setIsOpen3] = useState(false);
+
+  const AddFriend = () => {
+    setIsOpen3(true);
+  };
+
+  return (
+    <Container>
+      <PostPageTitle title="친구" />
+      <AddIcon onClick={AddFriend} />
+      {isOpen3 && (
+        <Modal onClose={() => setIsOpen3(false)}>
+          {/* <ApplicationFriendModal /> */}
+        </Modal>
+      )}
+    </Container>
+  );
+}
+
+export default FriendTop;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AddIcon = styled.div`
+  width: 30px;
+  height: 30px;
+  margin-bottom: 20px;
+  margin-left: 10px;
+  background-color: antiquewhite;
+  cursor: pointer;
+`;
--- yeoreum/src/pages/friend/index.tsx
+++ yeoreum/src/pages/friend/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import PostContainer from '../../components/board/PostContainer';
+import FriendTop from '../../components/friend/page/FriendTop';
+import UserSearch from '../../components/friend/page/UserSearch';
+import MyFriendList from '../../components/friend/page/MyFriendList';
 
 function index() {
-  return <PostContainer></PostContainer>;
+  return (
+    <PostContainer>
+      <FriendTop />
+      <UserSearch />
+      <MyFriendList />
+    </PostContainer>
+  );
 }
 
 export default index;
