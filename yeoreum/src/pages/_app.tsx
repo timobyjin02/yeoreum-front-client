5f66a71e76d8d0b40f47184df30232be2a1aefa7
ujin <dbwlsxkal95@gmail.com>
2023-02-07 15:12:05 +0900

    feat(feature/ujin/#69): react-query 초기 세팅

--- yeoreum/src/pages/_app.tsx
+++ yeoreum/src/pages/_app.tsx
@@ -6,15 +6,20 @@ import { ThemeProvider } from '@emotion/react';
 import theme from '../styles/theme';
 import Nav from '../components/nav/Nav';
 import Footer from '../components/footer/Footer';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <Nav />
-        <Component {...pageProps} />
-        <Footer />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Nav />
+          <Component {...pageProps} />
+          <Footer />
+        </Provider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
