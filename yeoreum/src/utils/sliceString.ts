e27fe061472afb8ed452a2cb66f8ef08a4e24156
donkey_kong <donghui8266@gmail.com>
2023-02-13 15:55:03 +0900

    refactor(feature/donkey/#78): sliceString 함수 수정

--- yeoreum/src/utils/sliceString.ts
+++ yeoreum/src/utils/sliceString.ts
@@ -3,5 +3,5 @@ type Limit = number;
 
 export default function sliceString(text: Text, limitLength: Limit) {
   if (text.length <= limitLength) return text;
-  else return text.substring(0, limitLength) + '...';
+  return text.substring(0, limitLength) + '...';
 }
