fad7486080571d68db9a7ab00ec4ff2dd93c0cc2
donkey_kong <donghui8266@gmail.com>
2023-02-14 20:21:47 +0900

    feat(feature/donkey/#80): 게시물 api 요청 구현

---
+++ yeoreum/src/api/posts.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export interface RequestGetAllPostsOption {
+  gender?: string | undefined;
+  people?: number | undefined;
+  isDone?: boolean | undefined;
+  isImpromptu?: boolean | undefined;
+}
+
+const requestGetPosts = (
+  pageParam: number,
+  option: RequestGetAllPostsOption,
+  token: string,
+) => {
+  const url = `/api/boards?${
+    option.gender === undefined ? '' : `gender=${option.gender}&`
+  }${option.people === undefined ? '' : `people=${option.people}&`}${
+    option.isDone === undefined ? '' : `isDone=${option.isDone}&`
+  }${
+    option.isImpromptu === undefined ? '' : `isImpromptu=${option.isDone}&`
+  }${`page=${pageParam}`}`;
+
+  return axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export { requestGetPosts };
