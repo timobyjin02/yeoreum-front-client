89f991480ff9663cbf43bf97b1f1832a8f1fd26b
donkey_kong <donghui8266@gmail.com>
2023-02-15 18:43:13 +0900

    feat(feature/donkey/#89): 브라우저 포커싱, fetching 실패 시 재요청 옵션 설정

--- yeoreum/src/hooks/queries/posts.ts
+++ yeoreum/src/hooks/queries/posts.ts
@@ -23,6 +23,10 @@ const usePostsInfiniteQuery = (
         if (page < totalPage) return pages.length + 1;
         return undefined;
       },
+      // retry 기본값 3은 fetching 실패시 재요청을 3번까지 하는데요~
+      // 때문에 error를 리턴하기까지 오래 걸려서 UX에 좋지 않은 영향을 줄 수 있어 1로 설정합니다~
+      retry: 1,
+      refetchOnWindowFocus: false,
     },
   );
 };
