e2b689947af128490dbbabb5a8705e08784d82ba
donkey_kong <donghui8266@gmail.com>
2023-02-21 20:05:57 +0900

    refactor(feature/donkey/#99): useMutation 옵션 타입 분리

--- yeoreum/src/hooks/queries/auth.ts
+++ yeoreum/src/hooks/queries/auth.ts
@@ -1,18 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
-import { AxiosResponse } from 'axios';
 import { requestPostLogin } from '../../apis/notices';
-
-type OnSuccess =
-  | ((
-      data: AxiosResponse<any, any>,
-      variables: void,
-      context: unknown,
-    ) => unknown)
-  | undefined;
-
-type OnError =
-  | ((error: any, variables: void, context: unknown) => unknown)
-  | undefined;
+import { OnError, OnSuccess } from '../../types/mutation';
 
 const useLoginMutation = (onSuccess: OnSuccess, onError: OnError) => {
   return useMutation(requestPostLogin, {
