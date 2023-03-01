b4e0e43be2ecff826ec777d750d2c80bc5022536
ujin <dbwlsxkal95@gmail.com>
2023-03-01 14:30:51 +0900

    publish(feature/ujin/#110): 배너 텍스트 추가

--- yeoreum/src/pages/index.tsx
+++ yeoreum/src/pages/index.tsx
@@ -11,12 +11,43 @@ export default function Home() {
         <meta name="description" content="Yeoreum" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <AA />
+      <BannerWrapper>
+        <MainBanner src="/banner.svg" />
+        <MainText>너의 여름은</MainText>
+        <SubText>여름과 청춘, 아직 만난 적 없는 너를 찾고 있어</SubText>
+      </BannerWrapper>
       <Previews />
       <AA />
     </div>
   );
 }
+
+const MainBanner = styled.img`
+  width: 100%;
+`;
+
+const BannerWrapper = styled.div`
+  position: relative;
+`;
+
+const MainText = styled.span`
+  position: absolute;
+  top: 45%;
+  left: 29%;
+  transform: translate(-50%, -50%);
+  font-size: 50px;
+  color: #ffffff;
+  font-weight: 700;
+`;
+
+const SubText = styled.span`
+  position: absolute;
+  top: 55%;
+  left: 33.3%;
+  transform: translate(-50%, -50%);
+  font-size: 20px;
+  color: #ffffff;
+`;
 
 const AA = styled.div`
   background-color: skyblue;
