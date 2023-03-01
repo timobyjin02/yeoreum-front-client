dde1489fe3e8698e6c7a148fd4114dbc46aec119
ujin <dbwlsxkal95@gmail.com>
2023-03-01 11:47:17 +0900

    feat(feature/ujin/#123): 친구 신청 받은, 보낸 목록 리스트 보여지게 구현

--- yeoreum/src/components/friend/page/requestList/ReceivedFriendsRequests.tsx
+++ yeoreum/src/components/friend/page/requestList/ReceivedFriendsRequests.tsx
@@ -1,7 +1,68 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { useFriendsReceivedQuery } from '../../../../hooks/queries/friends';
+import { ReceivedFriendsResponseType } from '../../../../types/friend';
+import sliceString from '../../../../utils/sliceString';
+import ProfileImage from '../../../common/ProfileImage';
 
-function ReceivedFriendsRequests() {
-  return <div>Requests</div>;
+function ReceivedFriendsRequest() {
+  const { data } = useFriendsReceivedQuery();
+  const receivedFriends = data?.data.response.receivedRequests;
+
+  return (
+    <>
+      {receivedFriends?.map(
+        (receivedFriend: ReceivedFriendsResponseType, index: number) => {
+          return (
+            <List key={index}>
+              <ImageWrapper>
+                <ProfileImage
+                  src={receivedFriend?.senderUserProfileImage}
+                  size={70}
+                />
+              </ImageWrapper>
+              <InfoWrapper>
+                <Nickname>{receivedFriend.senderUserNickname}</Nickname>
+                <Description>
+                  {sliceString(receivedFriend.senderUserDescription, 65)}
+                </Description>
+              </InfoWrapper>
+            </List>
+          );
+        },
+      )}
+    </>
+  );
 }
 
-export default ReceivedFriendsRequests;
+export default ReceivedFriendsRequest;
+
+const List = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 600px;
+  padding: 8px 20px 10px;
+  cursor: pointer;
+`;
+
+const ImageWrapper = styled.div`
+  margin-right: 15px;
+`;
+
+const InfoWrapper = styled.div`
+  width: calc(100% - 65px);
+  height: 100%;
+`;
+
+const Nickname = styled.div`
+  margin-bottom: 10px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.palette.font.headline};
+`;
+
+const Description = styled.div`
+  font-size: 14px;
+  letter-spacing: 0.6px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.palette.font.headline};
+`;
--- yeoreum/src/components/friend/page/requestList/SentFriendsRequests.tsx
+++ yeoreum/src/components/friend/page/requestList/SentFriendsRequests.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import { useFriendsSentQuery } from '../../../../hooks/queries/friends';
+import { SentFriendsResponseType } from '../../../../types/friend';
+import sliceString from '../../../../utils/sliceString';
+import ProfileImage from '../../../common/ProfileImage';
 
 function SentFriendsRequests() {
-  return <div>SentFriendRequests</div>;
+  const { data } = useFriendsSentQuery();
+  const sentFriends = data?.data.response.sentFriendRequests;
+
+  return (
+    <>
+      {sentFriends?.map(
+        (sentFriend: SentFriendsResponseType, index: number) => {
+          return (
+            <List key={index}>
+              <ImageWrapper>
+                <ProfileImage
+                  src={sentFriend?.receiverUserProfileImage}
+                  size={70}
+                />
+              </ImageWrapper>
+              <InfoWrapper>
+                <Nickname>{sentFriend.receiverUserNickname}</Nickname>
+                <Description>
+                  {sliceString(sentFriend.receiverUserDescription, 65)}
+                </Description>
+              </InfoWrapper>
+            </List>
+          );
+        },
+      )}
+    </>
+  );
 }
 
 export default SentFriendsRequests;
+
+const List = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 600px;
+  padding: 8px 20px 10px;
+  cursor: pointer;
+`;
+
+const ImageWrapper = styled.div`
+  margin-right: 15px;
+`;
+
+const InfoWrapper = styled.div`
+  width: calc(100% - 65px);
+  height: 100%;
+`;
+
+const Nickname = styled.div`
+  margin-bottom: 10px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.palette.font.headline};
+`;
+
+const Description = styled.div`
+  font-size: 14px;
+  letter-spacing: 0.6px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.palette.font.headline};
+`;
